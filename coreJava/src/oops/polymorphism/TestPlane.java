9411b2ab9d0746b30d8cebc2e959f174596e917b
sidduganeshsid <sidduganeshengineer@gmail.com>
2024-01-03 11:43:16 +0530

    adding

---
+++ coreJava/src/oops/polymorphism/TestPlane.java
@@ -0,0 +1,60 @@
+package oops.polymorphism;
+
+//package OOP's Concept.polymorphism;
+class Plane
+{
+ public void takeoff()
+ {
+     System.out.println("plane is taking off");
+ }
+ public void landing()
+ {
+     System.out.println("plane is landing");
+ }
+ public void fly()
+ {
+     System.out.println("plane is flying");
+ }
+}
+class PassengerPlane extends Plane
+{
+ public void fly()
+ {
+     System.out.println("Passenger plane flies at medium height");
+ }
+}
+class CargoPlane extends Plane
+{
+ public void fly()
+ {
+     System.out.println("CargoPlane files at lower height");
+ }
+}
+class FighterPlane extends Plane
+{
+ public void fly()
+ {
+     System.out.println("Fighterlane files at higher height");
+ }
+}
+class Airport
+{
+ public void permit(Plane plane)
+ {
+     plane.takeoff();
+     plane.fly();
+     plane.landing();
+ }
+}
+public class TestPlane {
+ public static void main(String[] args) {
+     CargoPlane cp = new CargoPlane();
+     PassengerPlane pp = new PassengerPlane();
+     FighterPlane fp = new FighterPlane();
+
+     Airport air = new Airport();
+     air.permit(cp);
+     air.permit(pp);
+     air.permit(fp);
+ }
+}
